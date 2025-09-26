fe205316b320c5b16e0c3ea2f8c182dae2551816
globalitsources <globalitsourcesreact@gmail.com>
2025-09-26 18:22:35 +0530

    Update axiosInstance.js

--- Client/src/axiosInstance.js
+++ Client/src/axiosInstance.js
@@ -1,8 +1,43 @@
+// import axios from "axios";
+
+// const axiosInstance = axios.create({
+//   //baseURL: "https://gitserp.in/api",
+//   baseURL: "http://localhost:8000/api",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// axiosInstance.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("token");
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
+// axiosInstance.interceptors.response.use(
+//   (response) => response,
+//   (error) => {
+//     if (error.response && error.response.status === 401) {
+//       console.warn("Unauthorized! Redirecting to login...");
+//       localStorage.removeItem("token");
+//       window.location.href = "/";
+//     }
+//     return Promise.reject(error);
+//   }
+// );
+
+// export default axiosInstance;
+
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  //baseURL: "https://gitserp.in/api",
-  baseURL: "http://localhost:8000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api",
   headers: {
     "Content-Type": "application/json",
   },
@@ -16,10 +51,9 @@ axiosInstance.interceptors.request.use(
     }
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
+
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
@@ -33,3 +67,4 @@ axiosInstance.interceptors.response.use(
 );
 
 export default axiosInstance;
+
